4fbe48a7807057c08c59773d68dfd06efc91df71
Victor Musienko <victor.m@noibu.com>
2024-08-26 10:55:55 -0400

    Initial commit

---
+++ Linear-electron/index.js
@@ -0,0 +1,46 @@
+function start(address, deps) {
+    const { app, BrowserWindow, shell } = deps;
+    function createWindow(address) {
+        const win = new BrowserWindow({
+            width: 1024,
+            height: 800,
+            frame: true,
+            transparent: true,
+            webPreferences: {
+                nodeIntegration: false,
+                contextIsolation: true,
+            },
+        });
+
+        win.webContents.on("will-navigate", handleExternalLinks);
+        win.webContents.on("new-window", handleExternalLinks);
+
+        win.loadURL(address);
+        return win;
+    }
+
+    function handleExternalLinks(event, url) {
+        const win = BrowserWindow.getFocusedWindow();
+
+        if (url !== win.webContents.getURL() && !url.includes("auth")) {
+            event.preventDefault();
+            shell.openExternal(url);
+        }
+    }
+
+    app.whenReady().then(() => createWindow(address));
+
+    app.on("window-all-closed", () => {
+        if (process.platform !== "darwin") {
+            app.quit();
+        }
+    });
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+}
+
+start("https://linear.app/", require("electron"));
