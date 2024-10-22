e90a053ca63e33a7ccbdd82462abec96cc57c126
Victor Musienko <victor.m@noibu.com>
2024-10-21 23:47:50 -0400

    gitlab context menu action to select reviewers automagically

--- Gitlab/index.ts
+++ Gitlab/index.ts
@@ -1,6 +1,8 @@
 import { BrowserWindow } from "electron";
 import { commonAppLifecycle } from "../common";
 
+type Maybe<A> = A | null;
+
 async function start(address: string) {
   const state: { win?: BrowserWindow } = {};
   const customItems = [
@@ -15,33 +17,56 @@ async function start(address: string) {
   state.win = await commonAppLifecycle(address, customItems);
 
   function generateMRScript() {
-    return () => {
+    return async () => {
       const outputSelector = "[data-testid=issuable-form-title-field]";
       const submitSelector = "[data-track-label=submit_mr]";
       const bodySelector = "[data-testid=issuable-form-description-field]";
-      const input: HTMLInputElement = document.querySelector(
+      const assignMeSelector = ".assign-to-me-link";
+      const reviewersSelector = ".js-reviewer-search";
+      const reviewersIds = [13096912, 11109949];
+      const reviewerSelector = (id: number) => `li[data-user-id="${id}"] > a`;
+      const input: Maybe<HTMLInputElement> = document.querySelector(
         ".branch-selector code",
-      )!;
-      const output: HTMLInputElement = document.querySelector(outputSelector)!;
-      const issueBody: HTMLInputElement = document.querySelector(bodySelector)!;
-      const saveButton: HTMLButtonElement =
-        document.querySelector(submitSelector)!;
-
-      let [user, type, domain, explanation, ticket] =
-        input.innerText.split("/");
+      );
+      const output: Maybe<HTMLInputElement> =
+        document.querySelector(outputSelector);
+      const issueBody: Maybe<HTMLInputElement> =
+        document.querySelector(bodySelector);
+      const saveButton: Maybe<HTMLButtonElement> =
+        document.querySelector(submitSelector);
+      const assignMeButton: Maybe<HTMLButtonElement> =
+        document.querySelector(assignMeSelector);
+      const reviewersInput: Maybe<HTMLButtonElement> =
+        document.querySelector(reviewersSelector);
+      let [_user, type, domain, explanation, ticket] =
+        input?.innerText?.split?.("/") || [];
       explanation = explanation.replaceAll("-", " ");
       ticket = ticket.toUpperCase();
       type = type[0].toUpperCase() + type.slice(1);
-      output.value = `${type}(${domain}): ${explanation}`;
-      issueBody.value = issueBody.value.replace(
-        /Fixes LINEAR_ISSUE_ID/gi,
-        `Fixes ${ticket}`,
-      );
-      saveButton?.click();
+      if (output) {
+        output.value = `${type}(${domain}): ${explanation}`;
+      }
+      if (issueBody) {
+        issueBody.value = issueBody.value.replace(
+          /Fixes LINEAR_ISSUE_ID/gi,
+          `Fixes ${ticket}`,
+        );
+      }
+      assignMeButton?.click?.();
+      reviewersInput?.click?.();
+      await new Promise((r) => setTimeout(r, 1500));
+
+      for (const reviewerId of reviewersIds) {
+        document
+          .querySelector<HTMLButtonElement>(reviewerSelector(reviewerId))
+          ?.click?.();
+        await new Promise((r) => setTimeout(r, 300));
+      }
+      saveButton?.click?.();
     };
   }
 }
 
-start(
+void start(
   "https://gitlab.com/noibu/unicron/-/merge_requests?scope=all&state=opened&author_username=sdwvit.noibu",
 );
