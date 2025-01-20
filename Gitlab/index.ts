5cac9f0e794a9725e729626af559e0ad795c8a62
Victor Musienko <victor.m@noibu.com>
2025-01-20 11:26:23 -0500

    skip assign revieweres

--- Gitlab/index.ts
+++ Gitlab/index.ts
@@ -16,15 +16,31 @@ async function start(address: string) {
   ].filter(Boolean);
   state.win = await commonAppLifecycle(address, customItems);
 
+  async function assignReviewers() {
+    return; // todo skip for now
+    const reviewersIds = [13096912, 11109949];
+    const reviewerSelector = (id: number) => `li[data-user-id="${id}"] > a`;
+    const reviewersSelector = ".js-reviewer-search";
+    const reviewersInput: Maybe<HTMLButtonElement> =
+      document.querySelector(reviewersSelector);
+    reviewersInput?.click?.();
+    await new Promise((r) => setTimeout(r, 1500));
+
+    for (const reviewerId of reviewersIds) {
+      document
+        .querySelector<HTMLButtonElement>(reviewerSelector(reviewerId))
+        ?.click?.();
+      await new Promise((r) => setTimeout(r, 300));
+    }
+  }
+
   function generateMRScript() {
     return async () => {
       const outputSelector = "[data-testid=issuable-form-title-field]";
       const submitSelector = "[data-track-label=submit_mr]";
       const bodySelector = "[data-testid=issuable-form-description-field]";
       const assignMeSelector = ".assign-to-me-link";
-      const reviewersSelector = ".js-reviewer-search";
-      const reviewersIds = [13096912, 11109949];
-      const reviewerSelector = (id: number) => `li[data-user-id="${id}"] > a`;
+
       const input: Maybe<HTMLInputElement> = document.querySelector(
         ".branch-selector code",
       );
@@ -36,8 +52,7 @@ async function start(address: string) {
         document.querySelector(submitSelector);
       const assignMeButton: Maybe<HTMLButtonElement> =
         document.querySelector(assignMeSelector);
-      const reviewersInput: Maybe<HTMLButtonElement> =
-        document.querySelector(reviewersSelector);
+
       let [_user, type, domain, explanation, ticket] =
         input?.innerText?.split?.("/") || [];
       explanation = explanation.replaceAll("-", " ");
@@ -53,15 +68,7 @@ async function start(address: string) {
         );
       }
       assignMeButton?.click?.();
-      reviewersInput?.click?.();
-      await new Promise((r) => setTimeout(r, 1500));
-
-      for (const reviewerId of reviewersIds) {
-        document
-          .querySelector<HTMLButtonElement>(reviewerSelector(reviewerId))
-          ?.click?.();
-        await new Promise((r) => setTimeout(r, 300));
-      }
+      await assignReviewers();
       saveButton?.click?.();
     };
   }
